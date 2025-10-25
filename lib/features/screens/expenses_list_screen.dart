31aa289c7fd95ae02815fe5d7bd744fdb43ead03
Slaychok <slachok@mail.ru>
2025-10-25 11:15:49 +0300

    +expenses_list_screen_changed

--- lib/features/screens/expenses_list_screen.dart
+++ lib/features/screens/expenses_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_6/features/models/expense.dart';
 import 'package:my_flutter_6/features/widgets/expenses_list_view.dart';
+import 'package:my_flutter_6/features/screens/images_screen.dart';
 
 class ExpensesListScreen extends StatelessWidget {
   final List<Expense> expenses;
@@ -16,20 +17,45 @@ class ExpensesListScreen extends StatelessWidget {
     this.onTap,
   }) : super(key: key);
 
+  void _openImagesScreen(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (ctx) => const ImagesScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Мои Расходы'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.photo_library),
+            onPressed: () => _openImagesScreen(context),
+            tooltip: 'Галерея изображений',
+          ),
+        ],
       ),
       body: ExpensesListView(
         expenses: expenses,
         onDelete: onDelete,
         onTap: onTap,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: onAdd,
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 'images_btn',
+            onPressed: () => _openImagesScreen(context),
+            child: const Icon(Icons.photo),
+            mini: true,
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: onAdd,
+            child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }

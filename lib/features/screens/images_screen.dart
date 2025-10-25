4e4f90d6166cb923520f304694581e670769fc68
Slaychok <slachok@mail.ru>
2025-10-25 11:39:24 +0300

    +images

--- lib/features/screens/images_screen.dart
+++ lib/features/screens/images_screen.dart
@@ -4,14 +4,13 @@ import 'package:cached_network_image/cached_network_image.dart';
 class ImagesScreen extends StatelessWidget {
   const ImagesScreen({Key? key}) : super(key: key);
 
-  // Список URL изображений
   final List<String> imageUrls = const [
-    'https://picsum.photos/400/300?random=1',
-    'https://picsum.photos/400/300?random=2',
-    'https://picsum.photos/400/300?random=3',
-    'https://picsum.photos/400/300?random=4',
-    'https://picsum.photos/400/300?random=5',
-    'https://picsum.photos/400/300?random=6',
+    'https://www.shutterstock.com/shutterstock/photos/1929328700/display_1500/stock-photo-bunch-of-bananas-ready-for-sale-at-the-market-1929328700.jpg',
+    'https://www.shutterstock.com/shutterstock/photos/2677223589/display_1500/stock-photo-orange-knitted-sweater-with-geometric-beige-pattern-and-high-ribbed-collar-womens-warm-winter-2677223589.jpg',
+    'https://www.shutterstock.com/shutterstock/photos/2587859633/display_1500/stock-photo-close-up-of-colorful-candy-gum-drops-2587859633.jpg',
+    'https://www.shutterstock.com/shutterstock/photos/2610703851/display_1500/stock-photo-bangkok-thailand-apr-iphone-series-comparing-and-testing-the-new-iphone-as-well-as-2610703851.jpg',
+    'https://www.shutterstock.com/shutterstock/photos/2583657853/display_1500/stock-photo-dnipro-ukraine-dec-a-white-sony-playstation-controller-and-playstation-console-on-a-2583657853.jpg',
+    'https://www.shutterstock.com/shutterstock/photos/2656907887/display_1500/stock-photo-dog-toys-isolated-on-white-background-2656907887.jpg',
   ];
 
   @override
